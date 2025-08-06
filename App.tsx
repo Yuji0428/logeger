a0cfe84d4870cfbe8dc8eeb13191e543cd5a2762
Yuji0428 <y-kawakami@jcmnet.co.jp>
2025-08-06 15:13:21 +0900

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,1 +1,13 @@
-// App entry with routing
+import { Routes, Route } from 'react-router-dom';
+
+function Home() {
+  return <h1 style={{ padding: 20 }}>🏁 logeger アプリへようこそ！</h1>;
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
